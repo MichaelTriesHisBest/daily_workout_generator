c3f4664585ce23cbfbb4a5eb582a09d6c188a5a3
Michael M <michael.mcgaw.jr@gmail.com>
2022-12-26 17:38:09 -0600

    updated readme to include to-do list, updated workout gen script to have environment variables and some basic exception handling

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# daily_workout_generator]
+# daily_workout_generator
 Helpin me stay in shape with some daily workout check-ins
 ### Description
 Python-based script that randomly selects a workout from a curated list of workouts + reps.
 Connects to RDS and saves workout + reps so I can summerize and feel decent about myself at the end of the year.
 
 
+### TODO list 
+- [x] Add workouts and tuples
+- [x] Add readme
+- [x] Setup env variables
+- [] Add RDS connection
+- [] Add flags
--- workout_generator.py
+++ workout_generator.py
@@ -1,35 +1,55 @@
 import random
 import boto3
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+port = os.getenv("PORT")
+username = os.getenv("USER")
+password = os.getenv("PASSWORD")
+dbname = os.getenv("DB_NAME")
+endpoint = os.getenv("ENDPOINT")
+region = os.getenv("REGION")
 
 ABDOMINAL_TUPLE = (150,250)
 LEG_WORKOUT_TUPLE = (75, 200)
 UPPERBODY_TUPLE = (75, 150)
 STAY_AND_HOLD_TUPLE = (150, 280)
-WORKOUT_DICT = {"crunches" : ABDOMINAL_TUPLE, "push-ups" : UPPERBODY_TUPLE,
- "leg raises" : ABDOMINAL_TUPLE, "bicycle kicks" : ABDOMINAL_TUPLE, "air squats" : LEG_WORKOUT_TUPLE, 
- "planking" : STAY_AND_HOLD_TUPLE, "hanging leg raises" : STAY_AND_HOLD_TUPLE, 
- "lunges" : LEG_WORKOUT_TUPLE }
+WORKOUT_DICT = {
+    "crunches" : ABDOMINAL_TUPLE,
+ "push-ups" : UPPERBODY_TUPLE,
+ "leg raises" : ABDOMINAL_TUPLE, 
+ "bicycle kicks" : ABDOMINAL_TUPLE,
+ "air squats" : LEG_WORKOUT_TUPLE, 
+ "planking" : STAY_AND_HOLD_TUPLE, 
+ "hanging leg raises" : STAY_AND_HOLD_TUPLE, 
+ "lunges" : LEG_WORKOUT_TUPLE 
+ }
 
 
-if __name__ == "__main__":
-    random_workout = random.choice(list(WORKOUT_DICT.keys()))
-    min_range, max_range = WORKOUT_DICT.get(random_workout)
-    if random_workout != "planking":
-        print(f"Hello! Todays workout will be {random_workout}, which you'll need to do {random.randrange(min_range, max_range)} of!")
-    else:
-        print(f"Hello! Todays workout will be {random_workout}, which you'll need to do {random.randrange(min_range, max_range)} seconds on each side!")
-        
-    connect_to_db()
-    insert_workout_and_sets()
-        
+
         
 def connect_to_db():
     return ""
 
-def insert_workout_and_sets():
+def insert_workout_and_sets(workout: str, reps: int):
     return ""
     
 def disconnect_from_db():
     return ""
     
+if __name__ == "__main__":
+    random_workout = random.choice(list(WORKOUT_DICT.keys()))
+    min_range, max_range = WORKOUT_DICT.get(random_workout)
+    arbitrary_reps = random.randrange(min_range, max_range)
+    if random_workout != "planking":
+        print(f"Hello! Todays workout will be {random_workout}, which you'll need to do {arbitrary_reps} of!")
+    else:
+        print(f"Hello! Todays workout will be {random_workout}, which you'll need to do {arbitrary_reps} seconds on each side!")
+    try:
+        connect_to_db()
+        insert_workout_and_sets(workout=random_workout, reps=arbitrary_reps)
+    except Exception as e:
+        print(f"L + ratio : {e}")
+        
